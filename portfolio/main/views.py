552886a10f7822d03cbee5f8438c4d604fc0de56
claydsoncoelho <119833742+claydsoncoelho@users.noreply.github.com>
2024-08-07 05:41:11 +1200

    first commit

---
+++ portfolio/main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Project, Tag
+
+def home(request):
+    projects = Project.objects.all()
+    tags = Tag.objects.all()
+    return render(request, "home.html", {"projects":projects, "tags":tags})
+
+def contact(request):
+    return render(request, "contact.html")
+
+def project(request, id):
+    return render(request, "project.html")
